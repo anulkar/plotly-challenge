6f06286a3118a5e10e3f0e455f5aae4f63e50024
Atul Nulkar <nulkar@gmail.com>
2020-03-31 21:24:51 -0400

    Clean version with comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,16 @@
+// URL that points to the json dataset on your localhost
 bellyButtonData = "data/samples.json";
 
+// Read the JSON dataset using d3
 d3.json(bellyButtonData).then(data => {
-
-        // Populate the Test Subject IDs in the dropdown list based on available data
+        // Call function to populate the Test Subject IDs on the HTML page
         populateTestSubjectIDs(data);
     });
-
+/*
+--------------------------------------------------------------------------------
+Function that populates the Test Subject IDs in a dropdown list on the HTML page
+--------------------------------------------------------------------------------
+*/
 function populateTestSubjectIDs(data)
 {
     // Get a reference to the Test Subject IDs dropdown list
@@ -20,31 +25,53 @@ function populateTestSubjectIDs(data)
             return name;
         });
     
+    // Grab the first Test Subject ID
     testSubjectID = data.names[0];
+    // Call function to display demographic data for the Test Subject ID
     displayDemographicInfo(data, testSubjectID);
+    // Call function to plot top ten OTUs for the Test Subject ID as a bar chart
     displayTopTenOTUs(data, testSubjectID);
+    // Call function to plot a bubble chart of the OTUs for the Test Subject ID
     displayBubbleChart(data, testSubjectID);
+    // Call function to plot a gauge chart of the weekly washing frequency of the individual
     displayGaugeChart(data, testSubjectID);
 }
 
+/*
+-------------------------------------------------------------------------------------
+Function to display/refresh the data and charts on the HTML page.
+Calls other functions when a new Test Subject ID is selected from the dropdown list.  
+-------------------------------------------------------------------------------------
+*/
 function refreshData(testSubjectID)
 {
     d3.json(bellyButtonData).then(data => {
-
+        // Call function to display demographic data for the Test Subject ID
         displayDemographicInfo(data, testSubjectID);
+        // Call function to plot top ten OTUs for the Test Subject ID as a bar chart
         displayTopTenOTUs(data, testSubjectID);
+        // Call function to plot a bubble chart of the OTUs for the Test Subject ID
         displayBubbleChart(data, testSubjectID);
+        // Call function to plot a gauge chart of the weekly washing frequency of the individual
         displayGaugeChart(data, testSubjectID);
     });
 }
 
+/*
+---------------------------------------------------------------------------------
+Function to display demographic metadata for the selected Test Subject ID
+---------------------------------------------------------------------------------
+*/
 function displayDemographicInfo(data, testSubjectID)
 {
+    // Get a reference to the panel element that displays the demographic data 
     var demographicInfo = d3.select("#sample-metadata");
 
+    // Retrieve the demographic metadata from the dataset based on matching Test Subject ID
     data.metadata.map(metaData => {
         if (metaData.id == testSubjectID)
             {
+                // Display metadata in paragraph elements on the HTML page 
                 demographicInfo.selectAll("p").remove();
                 demographicInfo.append("p").text("ID: " + metaData.id);
                 demographicInfo.append("p").text("Ethnicity: " + metaData.ethnicity);
@@ -57,8 +84,15 @@ function displayDemographicInfo(data, testSubjectID)
     });
 }
 
+/*
+-----------------------------------------------------------------------------
+Function to plot top 10 OTUs for the selected Test Subject ID as a bar chart
+-----------------------------------------------------------------------------
+*/
 function displayTopTenOTUs(data, testSubjectID)
 {
+    // Retrieve OTU IDs, sample values and OTU labels from the dataset based on matching Test Subject ID
+    // Store data into respective arrays
     data.samples.map(sample => {
         if (sample.id == testSubjectID)
             {
@@ -74,10 +108,13 @@ function displayTopTenOTUs(data, testSubjectID)
             }
     });
 
+    // Slice the arrays to grab the first 10 data points for each
+    // Reverse the arrays to display data in descending order (pre-sorted in JSON)
     otuIDs = otuIDs.slice(0, 10).reverse();
     sampleValues = sampleValues.slice(0, 10).reverse();
     otuLabels = otuLabels.slice(0, 10).reverse();
 
+    // Build the trace for the bar chart
     var trace = [{
         type: 'bar',
         x: sampleValues,
@@ -86,14 +123,24 @@ function displayTopTenOTUs(data, testSubjectID)
         orientation: 'h'
       }];
     
+    // Build the layout for the bar chart
     var layout = {
         title: ('Top ' + sampleValues.length + ' OTUs for Subject ID: ' + testSubjectID)
-    }  
+    }
+
+    // Render the bar chart using plotly
     Plotly.newPlot('bar', trace, layout);
 }
 
+/*
+-----------------------------------------------------------------------------
+Function to plot a bubble chart of all OTUs for the selected Test Subject ID
+-----------------------------------------------------------------------------
+*/
 function displayBubbleChart(data, testSubjectID)
 {
+    // Retrieve OTU IDs, sample values and OTU labels from the dataset based on matching Test Subject ID
+    // Store data into respective arrays
     data.samples.map(sample => {
         if (sample.id == testSubjectID)
             {
@@ -109,6 +156,7 @@ function displayBubbleChart(data, testSubjectID)
             }
     });
 
+    // Build the trace for the bubble chart
     var trace = [{
         x: otuIDs,
         y: sampleValues,
@@ -119,27 +167,36 @@ function displayBubbleChart(data, testSubjectID)
         },
         text: otuLabels
     }];
-            
-      var layout = {
-        title: 'Sample Size',
+    
+    // Build the layout for the bubble chart
+    var layout = {
+        title: 'All OTUs in the Sample',
     };
       
+    // Render the bubble chart using plotly
     Plotly.newPlot('bubble', trace, layout);
 }
 
+/*
+----------------------------------------------------------------------------------
+Function to plot a gauge chart of the weekly washing frequency of the individual
+----------------------------------------------------------------------------------
+*/
 function displayGaugeChart(data, testSubjectID)
 {
+    // Retrieve the washing frequency of the matching subject ID
     data.metadata.map(metaData => {
         if (metaData.id == testSubjectID) {
             washingFrequency = metaData.wfreq;
         }
     });
 
+    // Build the gauge chart
     var data = [{
         type: "indicator",
         mode: "gauge+number",
         value: washingFrequency,
-        title: { text: "Belly Button Washing Frequency" + "<br>" + "Scrubs per Week", font: { size: 22 } },
+        title: { text: "Belly Button Washing Frequency" + "<br>" + "(Scrubs per Week)", font: { size: 18 } },
         gauge: {
             axis: { range: [null, 9]},
             bar: { color: "orange" },
@@ -160,10 +217,12 @@ function displayGaugeChart(data, testSubjectID)
         }
     }];
     
+    // Build the layout for the gauge chart
     var layout = {
         margin: { t: 25, r: 25, l: 25, b: 25 },
         paper_bgcolor: "white",
       };
     
+    // Plot the gauge chart using plotly
     Plotly.newPlot('gauge', data, layout);    
 }
